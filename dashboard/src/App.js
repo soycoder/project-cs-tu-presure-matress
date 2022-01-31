45868c0f3ba432c88959543dfb3665d451a0e03c
apisith1 <43911669+soycoder@users.noreply.github.com>
2022-01-31 20:33:52 +0700

    Init web

---
+++ dashboard/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Sleep Posture Monitoring System to Identify Risky Positions of Pressure Ulcers.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
