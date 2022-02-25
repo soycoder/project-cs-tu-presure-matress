96e9051a081d1fb0cb085600114caa251c4cd45d
apisith1 <43911669+soycoder@users.noreply.github.com>
2022-02-25 21:18:35 +0700

    Update python + arduino (reader file)

--- board-32x16/board-32x16.ino
+++ board-32x16/board-32x16.ino
@@ -1,43 +1,29 @@
 // Arduino Uno
+// ! Analog signal (SIG_pin)
 #define SIG_pin A0
 #define s0 A4
 #define s1 A3
 #define s2 A2
 #define s3 A1
 
-//#define OUT_pin 2
-//#define w0 3
-//#define w1 4
-//#define w2 5
-//#define w3 6
+// ! Output signal (OUT_pin)
+// Top
+#define OUT_top_pin 2
+#define wt0 3
+#define wt1 4
+#define wt2 5
+#define wt3 6
 
-#define OUT_pin 9
-#define w0 10
-#define w1 11
-#define w2 12
-#define w3 13
+// Bottom
+#define OUT_bottom_pin 9
+#define wb0 10
+#define wb1 11
+#define wb2 12
+#define wb3 13
 
-//Mux control pins for analog signal (SIG_pin) default for arduino mini pro
-//const byte s0 = D0;
-//const byte s1 = D1;
-//const byte s2 = D2;
-//const byte s3 = D3;
-
-//Mux control pins for Output signal (OUT_pin) default for arduino mini pro
-//const byte w0 = D5; 
-//const byte w1 = D6;
-//const byte w2 = D7;
-//const byte w3 = D8;
-
-//Mux in "SIG" pin default for arduino mini pro 
-//const byte SIG_pin = A0; 
-
-//Mux out "SIG" pin default for arduino mini pro
-//const byte OUT_pin = D4;
-
-//Row and Column pins default for arduino mini pro
-//const byte STATUS_pin = 3;
-//const byte COL_pin = 2;
+#define ROW_SIZE 32
+#define COL_SIZE 16
+#define MAX_SIZE ROW_SIZE*COL_SIZE
 
 const boolean muxChannel[16][4]={
     {0,0,0,0}, //channel 0
@@ -62,11 +48,11 @@ const boolean muxChannel[16][4]={
 //incoming serial byte
 int inByte = 0;
 
-int valor = 0;               //variable for sending bytes to processing
-int calibra[16][16];         //Calibration array for the min values of each od the 225 sensors.
-int minsensor=254;          //Variable for staring the min array
+int valor = 0;                      // Variable for sending bytes to processing
+int calibra[ROW_SIZE][COL_SIZE];    // Calibration array for the min values of each od the {MAX_SIZE} sensors.
+int minsensor = 254;                // Variable for staring the min array
 int multiplier = 254;
-int pastmatrix[16][16];
+int pastmatrix[ROW_SIZE][COL_SIZE];
 
 void setup(){
     
@@ -75,15 +61,18 @@ void setup(){
   pinMode(s2, OUTPUT); 
   pinMode(s3, OUTPUT); 
   
-  pinMode(w0, OUTPUT); 
-  pinMode(w1, OUTPUT); 
-  pinMode(w2, OUTPUT); 
-  pinMode(w3, OUTPUT); 
+  pinMode(wt0, OUTPUT); 
+  pinMode(wt1, OUTPUT); 
+  pinMode(wt2, OUTPUT); 
+  pinMode(wt3, OUTPUT); 
+
+  pinMode(wb0, OUTPUT); 
+  pinMode(wb1, OUTPUT); 
+  pinMode(wb2, OUTPUT); 
+  pinMode(wb3, OUTPUT);
   
-  pinMode(OUT_pin, OUTPUT); 
-  
-//  pinMode(STATUS_pin, OUTPUT);
-//  pinMode(COL_pin, OUTPUT);
+  pinMode(OUT_top_pin, OUTPUT); 
+  pinMode(OUT_bottom_pin, OUTPUT); 
 
   
   digitalWrite(s0, LOW);
@@ -91,15 +80,18 @@ void setup(){
   digitalWrite(s2, LOW);
   digitalWrite(s3, LOW);
   
-  digitalWrite(w0, LOW);
-  digitalWrite(w1, LOW);
-  digitalWrite(w2, LOW);
-  digitalWrite(w3, LOW);
+  digitalWrite(wt0, LOW);
+  digitalWrite(wt1, LOW);
+  digitalWrite(wt2, LOW);
+  digitalWrite(wt3, LOW);
+
+  digitalWrite(wb0, LOW);
+  digitalWrite(wb1, LOW);
+  digitalWrite(wb2, LOW);
+  digitalWrite(wb3, LOW);
   
-  digitalWrite(OUT_pin, HIGH);
-//  digitalWrite(STATUS_pin, HIGH);
-//  digitalWrite(COL_pin, HIGH);
-  
+  digitalWrite(OUT_top_pin, HIGH);
+  digitalWrite(OUT_bottom_pin, HIGH);
  
   
   Serial.begin(115200);
@@ -107,25 +99,37 @@ void setup(){
   Serial.println("\n....Calibratong...\n");
   
   // Full of 0's of initial matrix
-  for(byte j = 0; j < 16; j ++){ 
-    writeMux(j);
-    for(byte i = 0; i < 16; i ++)
+  for(byte j = 0; j < ROW_SIZE; j ++){ 
+    if(j < ROW_SIZE/2)
+      writeTopMux(j);
+    if(j >= ROW_SIZE/2)
+      writeBottomMux(j-16);
+    
+    for(byte i = 0; i < COL_SIZE; i ++)
       calibra[j][i] = 0;
   }
   
   // Calibration
   for(byte k = 0; k < 50; k++){  
-    for(byte j = 0; j < 16; j ++){ 
-      writeMux(j);
-      for(byte i = 0; i < 16; i ++)
+    for(byte j = 0; j < ROW_SIZE; j ++){ 
+      if(j < ROW_SIZE/2)
+        writeTopMux(j);
+      if(j >= ROW_SIZE/2)
+        writeBottomMux(j-16);
+
+      for(byte i = 0; i < COL_SIZE; i ++)
         calibra[j][i] = calibra[j][i] + readMux(i);
     }
   }
   
   //Print averages
-  for(byte j = 0; j < 16; j ++){ 
-    writeMux(j);
-    for(byte i = 0; i < 16; i ++){
+  for(byte j = 0; j < ROW_SIZE; j ++){ 
+    if(j < ROW_SIZE/2)
+      writeTopMux(j);
+    if(j >= ROW_SIZE/2)
+      writeBottomMux(j-16);
+    
+    for(byte i = 0; i < COL_SIZE; i ++){
       calibra[j][i] = calibra[j][i]/50;
       if(calibra[j][i] < minsensor)
         minsensor = calibra[j][i];
@@ -139,7 +143,7 @@ void setup(){
 //  Serial.print("Minimum Value: ");
 //  Serial.println(minsensor);
 //  Serial.println();
-  
+//  
   establishContact();
  
 //  digitalWrite(COL_pin, LOW);
@@ -154,10 +158,13 @@ void loop(){
     
     if(inByte == 'A'){
     
-      for(int j = 15; j >= 0; j--){ 
-        writeMux(j);
+      for(int j = 0; j < ROW_SIZE; j++){ 
+        if(j < ROW_SIZE/2)
+          writeTopMux(j);
+        if(j >= ROW_SIZE/2)
+          writeBottomMux(j-16);
         
-        for(int i = 0; i < 16; i++){
+        for(int i = 0; i < COL_SIZE; i++){
             
           valor = readMux(i);
           
@@ -179,12 +186,11 @@ void loop(){
           Serial.print(valor);
           Serial.print("\t");
 //          Serial.write(valor);
-//          digitalWrite(COL_pin,!digitalRead(COL_pin));
         } 
 //        Serial.println();
       }
 //      Test Delay
-//      delay(2000);
+      delay(2000);
     }
         
   }
@@ -206,8 +212,17 @@ int readMux(byte channel){
   return val;
 }
 
-void writeMux(byte channel){
-  byte controlPin[] = {w0, w1, w2, w3};
+void writeTopMux(byte channel){
+  byte controlPin[] = {wt0, wt1, wt2, wt3};
+
+  //loop through the 4 sig
+  for(byte i = 0; i < 4; i ++){
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
+}
+
+void writeBottomMux(byte channel){
+  byte controlPin[] = {wb0, wb1, wb2, wb3};
 
   //loop through the 4 sig
   for(byte i = 0; i < 4; i ++){
@@ -218,6 +233,6 @@ void writeMux(byte channel){
 void establishContact() {
   while (Serial.available() <= 0) {
     Serial.print('A');   // send a capital A
-    delay(300);
+    delay(1000);
   }
 }
